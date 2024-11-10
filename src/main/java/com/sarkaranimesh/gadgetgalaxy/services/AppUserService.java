f14b3e533ad2051a8d4deaaee80d99fd276560f2
animeshsarkar08 <139778009+animeshsarkar08@users.noreply.github.com>
2024-11-10 21:06:22 +0530

    Update AppUserService.java

--- src/main/java/com/sarkaranimesh/gadgetgalaxy/services/AppUserService.java
+++ src/main/java/com/sarkaranimesh/gadgetgalaxy/services/AppUserService.java
@@ -1,6 +1,6 @@
 package com.sarkaranimesh.gadgetgalaxy.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+/*import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Service;
 import com.sarkaranimesh.gadgetgalaxy.models.AppUser;
 import com.sarkaranimesh.gadgetgalaxy.repositories.AppUserRepository;
 
-@Service
-public class AppUserService implements UserDetailsService {
+@Service*/
+public class AppUserService {//implements UserDetailsService {
 	
-	@Autowired
+/*	@Autowired
 	private AppUserRepository repo;
 	
 	@Override
@@ -33,4 +33,4 @@ public class AppUserService implements UserDetailsService {
 				
 	}
 
-}
+}*/
